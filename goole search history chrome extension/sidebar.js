5f894d830a41d63dfdfdbad5497633aa99c5e190
Oyekan Gbare <oyekanorisunmigbare@gmail.com>
2019-03-03 19:10:20 +0100

    feat : added favicons to google search history

--- goole search history chrome extension/sidebar.js
+++
@@ -1,4 +0,0 @@
-let sidebarUrl = chrome.extension.getURL('sidebar.html')
-let iframe = document.createElement('iframe'), sourceAttribute = document.createAttribute('src'), id = document.createAttribute('id');
-sourceAttribute.value = sidebarUrl; id.value = 'customSearchSaverSidebar97'; iframe.setAttributeNode(sourceAttribute);  iframe.setAttributeNode(id)
-document.getElementsByTagName('html')[0].appendChild(iframe)
--- goole search history chrome extension/update.js
+++ goole search history chrome extension/update.js
@@ -1,179 +1,118 @@
+{
+  let historyDiv = document.getElementById("googleHistory"),
+    googleReferredDiv = document.getElementById("googleReferred");
 
-let consoleHistory = document.getElementById('consoleHistory'), historyDiv = document.getElementById('googleHistory')
-  updateGoogleHistory()
-function updateGoogleHistory(){
-    historyDiv.innerHTML = ''
+  //  update required div with google search history
+  updateGoogleHistory();
+
+  // get google visted urls from chrome's local storage && update required div
+  chrome.storage.local.get(["googleVisitedUrls"], function(urlObject) {
+    console.log(urlObject);
+    getUpdate(urlObject.googleVisitedUrls, googleReferredDiv);
+  });
+
+  // refresh visited from google div when navigated to
+  let activator = document.getElementById("googleLinkedActivator");
+  activator.addEventListener("click", function() {
+    document.getElementById("googleReferred").innerHTML = "";
+    chrome.storage.local.get(["googleVisitedUrls"], function(urlObject) {
+      getUpdate(urlObject.googleVisitedUrls, googleReferredDiv);
+    });
+  });
+
+  function updateGoogleHistory() {
+    historyDiv.innerHTML = "";
     let historyItemsArray = [];
     // search chrome's history for visited url's containing the google search url
-    chrome.history.search({'text': '', startTime: 1134784305395}, function(HistoryItems){
-            // containing goole search url ? push it to historyItemsArray
-            HistoryItems.forEach(item => {
-                if(item.url.includes("https://www.google.com/search?q")){
-                     console.log(item)
-                     historyItemsArray.push(item)
-            }
-            })
-      
-        getUpdate(historyItemsArray,historyDiv)
+    chrome.history.search({ text: "", startTime: 1134784305395 }, function(
+      HistoryItems
+    ) {
+      // containing goole search url ? push it to historyItemsArray
+      HistoryItems.forEach(item => {
+        if (item.url.includes("https://www.google.com/search?q")) {
+          historyItemsArray.push(item);
+        }
+      });
 
-    
+      getUpdate(historyItemsArray, historyDiv);
     });
- 
-};
-
-function getUpdate(urlArray,targetDiv){
-    generateHeaderDate = (dateItem)=>{
-            let dateString
-            dateItem.lastVisitTime ?  dateString = new Date(parseInt(dateItem.lastVisitTime)).toDateString() : dateString = new Date(parseInt(dateItem.date)).toDateString()
-            return dateString
-    }
-    let ul = document.createElement('ul'), header = generateHeaderDate(urlArray[0]); h3 = document.createElement('h3'), headerContent = document.createTextNode(header);
-    h3.appendChild(headerContent)
-    ul.appendChild(h3)
-    urlArray.forEach((urlobj, i, arr)=>{
-
-			createLi(urlobj)
-    function createLi(urlobj){
-let itemDate = generateHeaderDate(urlobj) ;
-console.log(itemDate)
-        if (itemDate !== header){
-	console.log('starting new list')
-        if (ul.children.length == 0){
-console.log('ul has no children, so chaging the ul,date.. automatically')
-			console.log(ul.children.length)
-            ul = document.createElement('ul'); header =  generateHeaderDate(urlobj) ;  h3 = document.createElement('h3'); headerContent = document.createTextNode(header);
+  }
+  function getUpdate(urlArray, targetDiv) {
+    generateHeaderDate = dateItem => {
+      let dateString;
+      dateItem.lastVisitTime
+        ? (dateString = new Date(
+            parseInt(dateItem.lastVisitTime)
+          ).toDateString())
+        : (dateString = new Date(parseInt(dateItem.date)).toDateString());
+      return dateString;
+    };
+    let ul = document.createElement("ul"),
+      header = generateHeaderDate(urlArray[0]);
+    (h3 = document.createElement("h3")),
+      (headerContent = document.createTextNode(header));
+    h3.appendChild(headerContent);
+    ul.appendChild(h3);
+    urlArray.forEach((urlobj, i, arr) => {
+      createLi(urlobj);
+      function createLi(urlobj) {
+        let itemDate = generateHeaderDate(urlobj);
+        console.log(itemDate);
+        if (itemDate !== header) {
+          console.log("starting new list");
+          if (ul.children.length == 0) {
+            console.log(
+              "ul has no children, so chaging the ul,date.. automatically"
+            );
+            console.log(ul.children.length);
+            ul = document.createElement("ul");
+            header = generateHeaderDate(urlobj);
+            h3 = document.createElement("h3");
+            headerContent = document.createTextNode(header);
             h3.appendChild(headerContent);
             ul.appendChild(h3);
-        }else{
-		console.log('appennding ul to div')
+          } else {
+            console.log("appennding ul to div");
             targetDiv.appendChild(ul);
-             ul = document.createElement('ul'); header =  generateHeaderDate(urlobj) ; h3 = document.createElement('h3'); headerContent = document.createTextNode(header);
+            ul = document.createElement("ul");
+            header = generateHeaderDate(urlobj);
+            h3 = document.createElement("h3");
+            headerContent = document.createTextNode(header);
             h3.appendChild(headerContent);
             ul.appendChild(h3);
+          }
         }
-        }
-	console.log('creating li')
-        let li = document.createElement('li'), a = document.createElement('a'), href = document.createAttribute('href'); title = document.createTextNode(urlobj.title)
-        href.value = urlobj.url; 
+        console.log("creating li");
+        let li = document.createElement("li"),
+          img = document.createElement("img"),
+          favsrc = document.createAttribute("src"),
+          a = document.createElement("a"),
+          href = document.createAttribute("href");
+        (title = document.createTextNode(urlobj.title)),
+          (faviconUri = urlobj.url.includes("https://www.google.com/search?q")
+            ? "../assets/icons/googleSearch.png"
+            : null);
+        favsrc.value = faviconUri;
+        img.setAttributeNode(favsrc);
+        href.value = urlobj.url;
         a.setAttributeNode(href);
         a.appendChild(title);
+        li.appendChild(img);
         li.appendChild(a);
-        ul.appendChild(li)
-        if(i === arr.length-1 ){
-		console.log('appennding ul to last div')
-            targetDiv.appendChild(ul);
-             ul = document.createElement('ul'); header =  generateHeaderDate(urlobj) ; h3 = document.createElement('h3'); headerContent = document.createTextNode(header);
-            h3.appendChild(headerContent);
-            ul.appendChild(h3);
+        ul.appendChild(li);
+        if (i === arr.length - 1) {
+          console.log("appennding ul to last div");
+          targetDiv.appendChild(ul);
+          ul = document.createElement("ul");
+          header = generateHeaderDate(urlobj);
+          h3 = document.createElement("h3");
+          headerContent = document.createTextNode(header);
+          h3.appendChild(headerContent);
+          ul.appendChild(h3);
+        }
+      }
+    });
+  }
 }
-    }
-    })
-    
-}    
-
-let activator = document.getElementById('googleLinkedActivator')
- chrome.storage.local.get(['googleVisitedUrls'], function(urlObject){
-        console.log(urlObject)
-      getUpdate(urlObject.googleVisitedUrls, googleReferredDiv);
-    }) ;
-activator.addEventListener('click',function(){
-    document.getElementById('googleReferred').innerHTML = ''
-  chrome.storage.local.get(['googleVisitedUrls'], function(urlObject){
-      console.log(urlObject)
-    getUpdate(urlObject.googleVisitedUrls, googleReferredDiv);
-     })  
-})
- chrome.runtime.onMessage.addListener(function(request, sender, sendRespaonse){
-     if(request.updateOnStart === "update googe referred history"){
-         console.log('received message to update google linked')
-     chrome.locals.get(['googleVisitedUrls'], function(urlObject){
-        getUpdate(urlObject.googleVisitedUrls, googleReferredDiv);
-    })
-    }
-})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function update(urlArray){
-//     let googleReferredDiv = document.getElementById('googleReferred')
-//     let ul = document.createElement('ul'), header = new Date(parseInt(urlArray[0].date)).toDateString(), h3 = document.createElement('h3'), headerContent = document.createTextNode(header);
-//     h3.appendChild(headerContent)
-//     ul.appendChild(h3)
-// // 	googleReferredDiv.appendChild(ul)
-//     urlArray.forEach((urlobj, i, arr)=>{
-
-// 			createLi(urlobj)
-//     function createLi(urlobj){
-// let itemDate = new Date(parseInt(urlobj.date)).toDateString();
-// console.log(itemDate)
-//         if (itemDate !== header){
-// 	console.log('starting new list')
-//         if (ul.children.length == 0){
-// console.log('ul has no children, so chaging the ul,date.. automatically')
-// 			console.log(ul.children.length)
-//             ul = document.createElement('ul'); header = new Date(parseInt(urlobj.date)).toDateString() ; h3 = document.createElement('h3'); headerContent = document.createTextNode(header);
-//             h3.appendChild(headerContent);
-//             ul.appendChild(h3);
-//         }else{
-// 		console.log('appennding ul to div')
-//             googleReferredDiv.appendChild(ul);
-//              ul = document.createElement('ul'); header = new Date(parseInt(urlobj.date)).toDateString(); h3 = document.createElement('h3'); headerContent = document.createTextNode(header);
-//             h3.appendChild(headerContent);
-//             ul.appendChild(h3);
-//         }
-//         }
-// 	console.log('creating li')
-//         let li = document.createElement('li'), a = document.createElement('a'), href = document.createAttribute('href'); title = document.createTextNode(urlobj.title)
-//         href.value = urlobj.url; 
-//         a.setAttributeNode(href);
-//         a.appendChild(title);
-//         li.appendChild(a);
-//         ul.appendChild(li)
-//         if(i === arr.length-1 ){
-// 		console.log('appennding ul to last div')
-//             googleReferredDiv.appendChild(ul);
-//              ul = document.createElement('ul'); header = new Date(parseInt(urlobj.date)).toDateString(); h3 = document.createElement('h3'); headerContent = document.createTextNode(header);
-//             h3.appendChild(headerContent);
-//             ul.appendChild(h3);
-// }
-//     }
-//     })
-    
-// }
